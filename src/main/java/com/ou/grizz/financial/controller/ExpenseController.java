ce5f8d9160dc9c4702a514e3c09856d4ad3be944
laithhanna <laithhanna6@gmail.com>
2022-02-14 13:39:30 -0500

    Implemented the add expense feature. Implemented the html page that handles the add expense forum. Implemented the saveExpense method into both the expense controller and the expense service layers.

--- src/main/java/com/ou/grizz/financial/controller/ExpenseController.java
+++ src/main/java/com/ou/grizz/financial/controller/ExpenseController.java
@@ -3,11 +3,14 @@ package com.ou.grizz.financial.controller;
 import com.ou.grizz.financial.model.CustomUserDetails;
 import com.ou.grizz.financial.model.Expense;
 import com.ou.grizz.financial.service.ExpenseService;
+import com.ou.grizz.financial.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -18,10 +21,30 @@ public class ExpenseController {
     @Autowired
     private ExpenseService expenseService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/expenses")
-    public String userExpenses(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
-        List<Expense> userExpenses = expenseService.findAllExpensesByUserId(userDetails.getUserId()); //Remember userDetails represents the authenticated user
+    public String userExpenses(@AuthenticationPrincipal CustomUserDetails loggedInUser, Model model) {
+        List<Expense> userExpenses = expenseService.findAllExpensesByUserId(loggedInUser.getUserId()); //Remember userDetails represents the authenticated user
         model.addAttribute("userExpenses", userExpenses);
         return "expenses";
+    }
+
+    //method handler to show a form for the user to add a new expense
+    @GetMapping("/showNewExpenseForm")
+    public String showNewExpenseForm(Model model) {
+        Expense expense = new Expense();
+        model.addAttribute("expense",expense);
+        return "new_expense";
+    }
+
+    //method handler to save the new expense the was created in the showNewExpenseForm
+    @PostMapping("/saveExpense")
+    public String saveExpense(@AuthenticationPrincipal CustomUserDetails loggedInUser,
+                              @ModelAttribute("expense") Expense expense) {
+
+        expenseService.saveExpense(loggedInUser.getUserId(), expense);
+        return "redirect:/expenses";
     }
 }
--- src/main/java/com/ou/grizz/financial/service/ExpenseServiceImpl.java
+++ src/main/java/com/ou/grizz/financial/service/ExpenseServiceImpl.java
@@ -1,11 +1,16 @@
 package com.ou.grizz.financial.service;
 
 import com.ou.grizz.financial.model.Expense;
+import com.ou.grizz.financial.model.User;
 import com.ou.grizz.financial.repository.ExpenseRepository;
+import com.ou.grizz.financial.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 //TODO implement these methods
 @Service
@@ -14,6 +19,10 @@ public class ExpenseServiceImpl implements ExpenseService{
 
     @Autowired
     private ExpenseRepository expenseRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public List<Expense> findAllExpensesByUserId(Long userId) {
         return expenseRepository.findExpensesByUserid(userId);
@@ -26,6 +35,16 @@ public class ExpenseServiceImpl implements ExpenseService{
 
     @Override
     public void saveExpense(Long userId, Expense expense) {
+
+        Optional<User> optional = userRepository.findById(userId);
+        if (optional.isPresent()) {
+            User user = optional.get();
+            expense.setUser(user);
+            user.addExpense(expense);
+            userRepository.save(user);
+        } else {
+            throw new UsernameNotFoundException("This user is not found");
+        }
 
     }
 
